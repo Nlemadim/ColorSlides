7dc0eacd7f92d13b3c5991bf6f8c519a62a1c096
Tony Nlemadim <nlemadimtony@gmail.com>
2020-03-05 21:36:52 -0500

    Color Slides with Black, yellow, green disabled

--- Colormaker_Slides/ViewController.swift
+++ Colormaker_Slides/ViewController.swift
@@ -15,6 +15,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+//    Outlets
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSlide: UISlider!
+    @IBOutlet weak var whiteSlide: UISlider!
+    @IBOutlet weak var blackSlide: UISlider!
+    @IBOutlet weak var blueSlide: UISlider!
+    @IBOutlet weak var yellowSlide: UISlider!
+    @IBOutlet weak var greenSlide: UISlider!
+    
+//    Actions
+    
+    @IBAction func changeColorPallet(_ sender: AnyObject) {
+        
+        let r: CGFloat = CGFloat(self.redSlide.value)
+        let g: CGFloat = CGFloat(self.greenSlide.value)
+//        let y: CGFloat = CGFloat(self.yellowSlide.value)
+        let b: CGFloat = CGFloat(self.blueSlide.value)
+//        let black: CGFloat = CGFloat(self.blackSlide.value)
+//        let w: CGFloat = CGFloat(self.whiteSlide.value)
+       
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+    
 }
 
